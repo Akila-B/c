a6a09c4fc1932bb9b7eff919ae0943c7c7586303
Akila-B <akilakala97@gmail.com>
2017-06-30 10:36:49 +0530

    large.java

---
+++ large.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+class large
+{
+public static void main(String[] args)
+{
+Scanner sc=new scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+if(a>b)
+{
+if(a>c)
+{
+System.out println(a)'
+}
+else
+{
+System.out.println(c);
+}
+}
+else
+{
+System.out.println(b);
+}
+}
+}
